69a2c4a55cc62007de99a79a5214907bdf414bc3
mbunix <bellianotieno24@gmal.com>
2024-04-07 17:34:23 +0300

    (core):google-signin

---
+++ Web/viBank-Web/viBank-Api/viBank-Api/Controllers/Authcontroller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Google.Apis.Auth;
+namespace viBank_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Authcontroller : ControllerBase
+    {
+
+        private readonly AppDbContext _dbContext;
+        public Authcontroller(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("google")]
+        public IActionResult GoogleSignIn()
+        {
+            var authentictionProperties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action(nameof(HandleGoogleResponse))
+            };
+            return Challenge(authentictionProperties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("google-response")]
+        public async Task<IActionResult> HandleGoogleResponse(string idToken)
+        {
+            try
+            {
+
+                var payload = await GoogleJsonWebSignature.ValidateAsync(idToken);
+                var email = payload.Email;
+
+                // check if the user is in the databasse 
+                var existingUser = await _dbContext.User.FirstOrDefaultAsync(u => u.Email == email);
+                if (existingUser == null)
+                {
+                    // user does not exist so create  one
+
+
+                }
+
+            }
+
+        }
+    }
+}
--- Web/viBank-Web/viBank-Api/viBank-Api/Helpers/AuthenticationHelper.cs
+++ Web/viBank-Web/viBank-Api/viBank-Api/Helpers/AuthenticationHelper.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
 namespace viBank_Api.Helpers
 {
     public class AuthenticatedUser
