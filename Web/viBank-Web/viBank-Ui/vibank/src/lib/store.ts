52373bf9adf3b0bbb073ce8bc63e6b31accdc5ee
BELLIAN OTIENO <bellianotieno24@gmail.com>
2024-04-16 08:40:44 +0300

    feat: add-redux

---
+++ Web/viBank-Web/viBank-Ui/vibank/src/lib/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {  TypedUseSelectorHook, useSelector } from "react-redux";
+import { authReducer } from "./auth/Authslice";
+
+export const makeStore = () => {
+    return configureStore({
+        reducer:
+        {
+            auth: authReducer 
+                
+            },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+    })
+}
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
