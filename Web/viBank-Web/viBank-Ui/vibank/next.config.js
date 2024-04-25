4cdc594c52cfed8ab92b5d9fdf6af4e775652ea3
BELLIAN OTIENO <bellianotieno24@gmail.com>
2024-04-25 09:42:52 +0300

    changes-to-env

--- Web/viBank-Web/viBank-Ui/vibank/next.config.js
+++ Web/viBank-Web/viBank-Ui/vibank/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
 
-export default nextConfig
+  publicRuntimeConfig: {
+    viBankUrl:
+      process.env.APP_ENV === 'production'
+        ? process.env.VIBANK_API_URL
+        : 'http://localhost:5000'
+  },
+  pageExtensions: ['tsx'],
+  webpack (config, options) {
+    config.resolve.alias['pagesDir'] = path.join(
+      __dirname,
+      'Web/viBank-Web/viBank-Ui/vibank/src/pages'
+    )
+    return config
+  }
+}
+module.exports = nextConfig
