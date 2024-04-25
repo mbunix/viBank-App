20884e368c59b6c7202efd0cbb3d774e2a70ed36
BELLIAN OTIENO <bellianotieno24@gmail.com>
2024-04-25 10:42:15 +0300

    trial

--- Web/viBank-Web/viBank-Ui/vibank/tailwind.config.ts
+++ Web/viBank-Web/viBank-Ui/vibank/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
-const config : Config={
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,10 +13,15 @@ const config : Config={
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
 };
 
 export default config;
-
