9765ba4e9d8974640b8d3ea610b5cf82b2f628af
BELLIAN OTIENO <bellianotieno24@gmail.com>
2024-04-25 09:03:39 +0300

    pages-directory

--- Web/viBank-Web/viBank-Ui/vibank/next.config.js
+++ Web/viBank-Web/viBank-Ui/vibank/next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['tsx'],
-  webpack (config, options) {
-    config.resolve.alias['pagesDir'] = path.join(
-      __dirname,
-      'Web/viBank-Web/viBank-Ui/vibank/src/pages/index.tsx'
-    )
-    return config
-  }
-}
+const nextConfig = {}
 
 export default nextConfig
