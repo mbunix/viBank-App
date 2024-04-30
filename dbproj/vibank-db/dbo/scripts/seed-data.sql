5089f1fe5b76ac38ab3ee4a470a6fc4b6d80f3f8
BELLIAN OTIENO <bellianotieno24@gmail.com>
2024-04-30 09:56:01 +0300

    seed-data-changes

--- dbproj/vibank-db/dbo/scripts/seed-data.sql
+++ dbproj/vibank-db/dbo/scripts/seed-data.sql
@@ -1,41 +1,61 @@
-BEGIN
-    TRANSACTION;
-    SET IDENTITY_INSERT [DBO].[ATMS] ON;
-    GO WITH AVAILABLEATMS AS (
-        SELECT
-            *
-        FROM
-            (VALUES (1,
+BEGIN TRANSACTION;
+
+SET IDENTITY_INSERT [dbo].[ATMs] ON;
+
+WITH AVAILABLEATMS AS (
+    SELECT
+    *
+    FROM
+    (VALUES (1,
             '758bb331-e613-46bb-82c4-9191ec2ee826',
             'TorontoCA',
             2000000.00,
+            '858bb331-e613-46bb-82c4-9191ec2f826',
             1,
             GETDATE(),
-            0) ) AS VAL (ID,
-            ATMID,
-            LOCATION,
-            AVAILABLEBALANCE,
-            ISACTIVE,
-            CREATEDDTM,
-            ISDELETED)
-    ) MERGE [DBO].[ATMS] AS TARGET USING AVAILABLEATMS AS SOURCE ON SOURCE.ID = TARGET.ID WHEN NOT MATCHED BY TARGET THEN
-        INSERT (
-            [ID],
-            [ATMID],
-            [LOCATION],
-            [AVAILABLEBALANCE],
-            [ISACTIVE],
-            [CREATEDDTM],
-            [ISDELETED]
-        ) VALUES (
-            SOURCE.[ID],
-            SOURCE.[ATMID],
-            SOURCE.[LOCATION],
-            SOURCE.[AVAILABLEBALANCE],
-            SOURCE.[ISACTIVE],
-            SOURCE.[CREATEDDTM],
-            SOURCE.[ISDELETED]
-        )
-            WHEN MATCHED THEN UPDATE SET TARGET.[ATMID] = SOURCE.[ATMID], TARGET.[LOCATION] = SOURCE.[LOCATION], TARGET.[AVAILABLEBALANCE] = SOURCE.[AVAILABLEBALANCE], TARGET.[ISACTIVE] = SOURCE.[ISACTIVE], TARGET.[CREATEDDTM] = SOURCE.[CREATEDDTM], TARGET.[ISDELETED] = SOURCE.[ISDELETED];
-        GO SET IDENTITY_INSERT [DBO].[ATMS] OFF;
-        COMMIT TRANSACTION;
+            0)
+        ) AS VAL (ID,
+                ATMID,
+                Location,
+                AvailableBalance,
+                TransactionID,
+                IsActive,
+                CreatedDTM,
+                IsDeleted)
+)
+MERGE [dbo].[ATMs] AS TARGET
+USING AVAILABLEATMS AS SOURCE
+ON SOURCE.ID = TARGET.ID
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (
+                [ATMID]
+                [Location]
+                [AvailableBalance]
+                [TransactionID]
+                [IsActive]
+                [CreatedDTM]
+                [IsDeleted]
+    )
+    VALUES (
+    SOURCE.[ID],
+    SOURCE.[ATMID],
+    SOURCE.[Location],
+    SOURCE.[AvailableBalance],
+    SOURCE.[TransactionID]
+    SOURCE.[IsActive],
+    SOURCE.[CreatedDTM],
+    SOURCE.[ISDeleted]
+    )
+WHEN MATCHED THEN
+    UPDATE
+    SET TARGET.[ATMID] = SOURCE.[ATMID],
+    TARGET.[Location] = SOURCE.[Location],
+    TARGET.[AvailableBalance] = SOURCE.[AvailableBalance],
+    TARGET.[TransactionID] = SOURCE.[TransactionID],
+    TARGET.[IsActive] = SOURCE.[IsActive],
+    TARGET.[CreatedDTM] = SOURCE.[CreatedDTM],
+    TARGET.[IsDeleted] = SOURCE.[IsDeleted];
+
+SET IDENTITY_INSERT [dbo].[ATMs] OFF;
+
+COMMIT TRANSACTION;
--- dbproj/vibank-db/dbo/tables/ATMs.sql
+++ dbproj/vibank-db/dbo/tables/ATMs.sql
@@ -3,13 +3,13 @@ CREATE TABLE [dbo].[ATMs]
      [ID]              BIGINT           IDENTITY (1, 1) NOT NULL,
     [ATMID]           UNIQUEIDENTIFIER NOT NULL,
     [Location]        NVARCHAR (MAX)   NOT NULL,
-    [AvailbleBalance] DECIMAL (18, 2)  NOT NULL,
+    [AvailableBalance] DECIMAL (18, 2)  NOT NULL,
     [TransactionID]   UNIQUEIDENTIFIER NOT NULL,
     [isActive]        BIT              NOT NULL,
     [isDeleted]       BIT              NOT NULL,
     [CreatedDTM]      DATETIME2 (7)    NOT NULL,
-    [UpdatedDTM]      DATETIME2 (7)    NOT NULL,
-    [CreatedBy]       BIGINT           NOT NULL,
-    [UpdatedBy]       BIGINT           NOT NULL,
+    [UpdatedDTM]      DATETIME2 (7)     NULL,
+    [CreatedBy]       BIGINT            NULL,
+    [UpdatedBy]       BIGINT           NULL,
     CONSTRAINT [PK_ATMs] PRIMARY KEY CLUSTERED ([ATMID] ASC)
 )
